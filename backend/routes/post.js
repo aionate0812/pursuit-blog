4f0e083c7d4b75b82d898ebfaf7c768964e31887
Alexander <aionate0812@gmail.com>
2019-01-26 04:17:10 -0500

    more code

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -32,10 +32,36 @@ const checkForToken=(req,res,next)=>{
 
     //UPDATE POST
 
-    postRouter.put('/:id',(req,res)=>{
-        
+    postRouter.put('/:id',checkForToken,(req,res)=>{
+        const {id} = req.params
+       const {title,body} = req.body
+        postService.updatePost(id,title,body)
+        .then(()=>{
+            res.json({response:'post updated'})
+        })
     })
 
+    //DELETE POST 
+    postRouter.delete('/:id',checkForToken,(req,res)=>{
+        const {id} = req.params
+        postService.deletePost(id).then(()=>res.json({response:'post deleted'}))
 
+    })
 
+//GET COMMENTS ON POST
+postRouter.get('/:id/comments',(req,res)=>{
+    const {id} = req.params
+
+    postService.getCommentsOnPost(id)
+    .then((data)=>{
+res.json(data)
+    })
+})
+
+//GET COMMENT ON POST BY ID
+
+postRouter.get('/:id/comments/:comment_id',(req,res)=>{
+    const {comment_id} = req.params
+    postService.getCommmentOnPostById(comment_id)
+})
     module.exports = postRouter
